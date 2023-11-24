d4c11acd21d6ac276490a6d67d4ae5282d57cbcf
Adeline Alaskarov <adelinealaskarov@Adelines-MacBook-Air-2.local>
2023-11-23 20:06:45 -0500

    bug fix

--- utilities/authentication/SignInWithGoogle.js
+++ utilities/authentication/SignInWithGoogle.js
@@ -1,5 +1,5 @@
 import { signInWithPopup, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-auth.js";
-import {  } from "../../firebaseConfig.js";
+import { auth } from "../../firebaseConfig.js";
 
 // Function handles the users signin with their google account 
 export async function signInUserWithGoogle () {
